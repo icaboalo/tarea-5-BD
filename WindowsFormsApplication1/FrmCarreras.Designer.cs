5ad28159d28b615afe5bfd71a67113c6ec7d3ae7
sdist <sdist@itam.mx>
2018-10-12 13:25:58 -0500

    Cambios iñaki

--- WindowsFormsApplication1/FrmCarreras.Designer.cs
+++ WindowsFormsApplication1/FrmCarreras.Designer.cs
@@ -30,9 +30,13 @@
         {
             this.cbCarrera = new System.Windows.Forms.ComboBox();
             this.cbCiudad = new System.Windows.Forms.ComboBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgCarrera = new System.Windows.Forms.DataGridView();
             this.btBuscar = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dtServicios = new System.Windows.Forms.DataGridView();
+            this.dgInstituciones = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCarrera)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtServicios)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgInstituciones)).BeginInit();
             this.SuspendLayout();
             // 
             // cbCarrera
@@ -51,13 +55,13 @@
             this.cbCiudad.Size = new System.Drawing.Size(121, 21);
             this.cbCiudad.TabIndex = 1;
             // 
-            // dataGridView1
+            // dgCarrera
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 40);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(453, 209);
-            this.dataGridView1.TabIndex = 2;
+            this.dgCarrera.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgCarrera.Location = new System.Drawing.Point(13, 40);
+            this.dgCarrera.Name = "dgCarrera";
+            this.dgCarrera.Size = new System.Drawing.Size(158, 101);
+            this.dgCarrera.TabIndex = 2;
             // 
             // btBuscar
             // 
@@ -67,20 +71,41 @@
             this.btBuscar.TabIndex = 3;
             this.btBuscar.Text = "Buscar";
             this.btBuscar.UseVisualStyleBackColor = true;
+            this.btBuscar.Click += new System.EventHandler(this.btBuscar_Click);
+            // 
+            // dtServicios
+            // 
+            this.dtServicios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtServicios.Location = new System.Drawing.Point(195, 40);
+            this.dtServicios.Name = "dtServicios";
+            this.dtServicios.Size = new System.Drawing.Size(158, 101);
+            this.dtServicios.TabIndex = 4;
+            // 
+            // dgInstituciones
+            // 
+            this.dgInstituciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgInstituciones.Location = new System.Drawing.Point(13, 148);
+            this.dgInstituciones.Name = "dgInstituciones";
+            this.dgInstituciones.Size = new System.Drawing.Size(340, 101);
+            this.dgInstituciones.TabIndex = 5;
             // 
             // FrmCarreras
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(478, 261);
+            this.Controls.Add(this.dgInstituciones);
+            this.Controls.Add(this.dtServicios);
             this.Controls.Add(this.btBuscar);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgCarrera);
             this.Controls.Add(this.cbCiudad);
             this.Controls.Add(this.cbCarrera);
             this.Name = "FrmCarreras";
             this.Text = "FrmCarreras";
             this.Load += new System.EventHandler(this.FrmCarreras_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCarrera)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtServicios)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgInstituciones)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -89,7 +114,9 @@
 
         private System.Windows.Forms.ComboBox cbCarrera;
         private System.Windows.Forms.ComboBox cbCiudad;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgCarrera;
         private System.Windows.Forms.Button btBuscar;
+        private System.Windows.Forms.DataGridView dtServicios;
+        private System.Windows.Forms.DataGridView dgInstituciones;
     }
 }
--- WindowsFormsApplication1/FrmCarreras.cs
+++ WindowsFormsApplication1/FrmCarreras.cs
@@ -12,7 +12,7 @@ namespace WindowsFormsApplication1
 {
     public partial class FrmCarreras : Form
     {
-        DataSet dsGeneral = new DataSet();
+        DataSet dsCiudades = new DataSet(), dsCarreras = new DataSet(), dsCarrera = new DataSet(), dsServicios = new DataSet(), dsInstituciones;
         GestorBD.GestorBD gestorBD;
 
         public FrmCarreras()
@@ -20,13 +20,19 @@ namespace WindowsFormsApplication1
             InitializeComponent();
         }
 
+        private void btBuscar_Click(object sender, EventArgs e)
+        {
+
+        }
+
         private void FrmCarreras_Load(object sender, EventArgs e)
         {
             gestorBD = new GestorBD.GestorBD("MSDAORA", "bd07", "fercab", "oracle");
-            gestorBD.consBD("select NomCarr from carr", dsGeneral, "Carreras");
-            gestorBD.consBD("select distinct ciudad from campus", dsGeneral, "Ciudades");
-            Varios.Comunes.cargaCombo(cbCiudad, dsGeneral, "Carreras", "NomCarr");
-            Varios.Comunes.cargaCombo(cbCarrera, dsGeneral, "Ciudades", "ciudad");
+
+            gestorBD.consBD("select distinct ciudad from campus", dsCiudades, "Ciudades");
+            gestorBD.consBD("select Nomcarr from carr", dsCarreras, "Carreras");
+            Varios.Comunes.cargaCombo(cbCarrera, dsCarreras, "Carreras", "Nomcarr");
+            Varios.Comunes.cargaCombo(cbCiudad, dsCiudades, "Ciudades", "ciudad");
         }
     }
 }
